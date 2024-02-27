fd5ae5a102d67bd6be52d102ef5d491ab7d5be94
Mingxuan Fu <mfuu0003@student.monash.edu>
2024-02-27 22:14:13 +1100

    add basic sleep logging method

--- app/src/main/java/com/example/sleep_application/ui/sleep_tracking_timer/SleepTrackingViewModel.java
+++ app/src/main/java/com/example/sleep_application/ui/sleep_tracking_timer/SleepTrackingViewModel.java
@@ -1,4 +1,4 @@
-package com.example.sleep_application.ui.gallery;
+package com.example.sleep_application.ui.sleep_tracking_timer;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -6,14 +6,14 @@ import androidx.lifecycle.ViewModel;
 
 public class SleepTrackingViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> timerText;
 
     public SleepTrackingViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        timerText = new MutableLiveData<>();
+        timerText.setValue("This is gallery fragment");
     }
 
     public LiveData<String> getText() {
-        return mText;
+        return timerText;
     }
 }
