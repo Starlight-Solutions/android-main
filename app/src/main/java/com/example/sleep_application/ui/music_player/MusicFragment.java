af2e985a5541f0b968d0c5f909a5abcdf8896257
Cyrus <yloo0010@student.monash.edu>
2024-03-08 23:17:26 +1100

    implemented basic music player with couple demo tracks

---
+++ app/src/main/java/com/example/sleep_application/ui/music_player/MusicFragment.java
@@ -0,0 +1,116 @@
+package com.example.sleep_application.ui.music_player;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.sleep_application.R;
+import com.example.sleep_application.databinding.FragmentMusicBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicFragment extends Fragment {
+
+    private FragmentMusicBinding binding;
+    private MediaPlayer mediaPlayer;
+
+    private Integer currentMusicNumber = 0;
+    final private int minMusicNumber = 0;
+    final private int maxMusicNumber = 6;
+
+    List<Integer> musicList = new ArrayList<>();
+
+    private String currentTrack = "Current Track: ";
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        MusicViewModel loginViewModel = new ViewModelProvider(this).get(MusicViewModel.class);
+
+        binding = FragmentMusicBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        TextView textView = binding.textMusic;
+        loginViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        musicListSetup();
+
+        binding.playBtn.setOnClickListener(this::onClickPlay);
+        binding.stopBtn.setOnClickListener(this::onClickStop);
+        binding.pauseBtn.setOnClickListener(this::onClickPause);
+
+        binding.previousBtn.setOnClickListener(this::onClickPrevious);
+        binding.nextBtn.setOnClickListener(this::onClickNext);
+
+        return root;
+    }
+
+    public void onClickPlay(View view) {
+        if ( !mediaPlayer.isPlaying() ) {
+            mediaPlayer.start();
+            binding.textMusic.setText(currentTrack.concat(currentMusicNumber.toString()));
+        }
+    }
+
+    public void onClickPause(View view) {
+        if ( mediaPlayer.isPlaying() ) {
+            mediaPlayer.pause();
+            binding.textMusic.setText("Paused");
+        }
+    }
+
+    public void onClickStop(View view) {
+        mediaPlayer.stop();
+        binding.textMusic.setText("Stopped");
+    }
+
+
+    public void onClickPrevious(View view) {
+        mediaPlayer.stop();
+        if (currentMusicNumber > minMusicNumber) {
+            currentMusicNumber--;
+        }
+        binding.textMusic.setText(currentMusicNumber.toString());
+        mediaPlayer = MediaPlayer.create(getActivity(), musicList.get(currentMusicNumber));
+    }
+
+    public void onClickNext(View view) {
+        mediaPlayer.stop();
+        if (currentMusicNumber < maxMusicNumber) {
+            currentMusicNumber++;
+        }
+        binding.textMusic.setText(currentMusicNumber.toString());
+        mediaPlayer = MediaPlayer.create(getActivity(), musicList.get(currentMusicNumber));
+    }
+
+    private void musicListSetup() {
+        musicList.add(R.raw.m00);
+        musicList.add(R.raw.m01);
+        musicList.add(R.raw.m02);
+        musicList.add(R.raw.m03);
+        musicList.add(R.raw.m04);
+        musicList.add(R.raw.m05);
+        musicList.add(R.raw.m06);
+
+        // Initialize MediaPlayer with an audio file
+        mediaPlayer = MediaPlayer.create(getActivity(), musicList.get(currentMusicNumber));
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
+}
