3065b7034cdcdf82f63120c59bef5b5b2ef1c5cd
Cyrus <yloo0010@student.monash.edu>
2024-05-01 00:44:50 +1000

    added analysis and feedback on page

--- app/src/main/java/com/example/sleep_application/ui/sleep_tips/TipsFragment.java
+++ app/src/main/java/com/example/sleep_application/ui/sleep_tips/TipsFragment.java
@@ -1,6 +1,6 @@
 package com.example.sleep_application.ui.sleep_tips;
 
-import androidx.lifecycle.ViewModelProvider;
+import static java.lang.Math.round;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -16,17 +16,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.sleep_application.R;
 import com.example.sleep_application.database.LocalSqlDbService;
 import com.example.sleep_application.database.entity.SleepEntity;
-import com.example.sleep_application.databinding.FragmentMusicBinding;
 import com.example.sleep_application.databinding.FragmentTipsBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class TipsFragment extends Fragment {
 
@@ -53,65 +50,64 @@ public class TipsFragment extends Fragment {
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
         String email = sharedPref.getString("login_email", "No email");
         sleepData = new ArrayList<>(dbService.sleepDao().getAllByUser(email));
-        Log.d("TipsFrag", Integer.toString(sleepData.size()));
 
+        binding.textView2.setText(analysis());
 
-
-        binding.tipsButton.setOnClickListener(this::onClickTips);
-        binding.tempButton.setOnClickListener(this::onClickTemp);
-        binding.tempButton2.setOnClickListener(this::onClickTemp2);
-
-
-
+        binding.undersleepBtn.setOnClickListener(this::onClickDebugUnderSleep);
+        binding.goodsleepBtn.setOnClickListener(this::onClickDebugAvgSleep);
+        binding.oversleepBtn.setOnClickListener(this::onClickDebugOverSleep);
 
         return root;
     }
 
-    public void onClickTips(View view) {
+    public String analysis(){
         int sleep_size = sleepData.size();
         long seven = sleepData.stream().filter(entity -> 25200 > entity.getDuration()).count();
         long nine = sleepData.stream().filter(entity -> 32400 < entity.getDuration()).count();
-        long normal = sleepData.stream().filter(entity -> 25200 > entity.getDuration() && 32400 < entity.getDuration()).count();
+        long normal = sleep_size - seven - nine;
         long total_sleep_duration = 0;
         for (SleepEntity entity : sleepData) { total_sleep_duration += entity.getDuration(); }
         double avg = (double) total_sleep_duration / (double) sleep_size / 3600;
-        Snackbar.make(view, new StringBuffer("total:" + sleep_size + "  >7:" + seven + " <9:" + nine + " normal:" + normal + "avg(hrs):" + avg), Snackbar.LENGTH_SHORT).show();
-        // OO% of your sleeps have desirable length.
-        // OO% of your sleeps are less than 7 hours.
-        // OO% of your sleeps are more than 9 hours.
-        // OO% of your sleeps are outside your regular sleep pattern. (outliers, 3hrs+- outside avg)
 
-        // if sleep_size <= 0, not enough record
-        // if <7 >40% => sleep more, try relaxation
-        // if >9 >40% => sleep less, (tips to wake up)
-        // if both or outliers >40% => need regular sleep
-        // else good sleep
+        double goodsleep_hrs_p  = (double) normal / (double) sleep_size;
+        double undersleep_hrs_p = (double) seven / (double) sleep_size;
+        double oversleep_hrs_p  = (double) nine / (double) sleep_size;
+
+        String analysis = "";
+
+        if (sleep_size <= 0) {  // if sleep_size <= 0, not enough record
+            analysis += "You have no sleep record.\nGo get some sleep.";
+        } else {
+            analysis += "In total of " + sleep_size + " sleep record:\n";
+            analysis += "You have average of " + (double) (((int) (avg * 100)))/100 + " hrs of sleep.\n";
+            analysis += (int) (goodsleep_hrs_p * 100) + "% of your sleeps have desirable length.\n";
+            analysis += (int) (undersleep_hrs_p * 100) + "% of your sleeps are less than 7 hours.\n";
+            analysis += (int) (oversleep_hrs_p * 100) + "% of your sleeps are more than 9 hours.\n";
+            if ((undersleep_hrs_p > 0.4) && (oversleep_hrs_p > 0.4)) {
+                analysis += "Your sleep is irregular.\nConsider fixed sleep pattern.";
+            } else if (undersleep_hrs_p > 0.4) {
+                analysis += "You need more sleep, try some relaxation methods.";
+            } else if (oversleep_hrs_p > 0.4) {
+                analysis += "You are sleeping too much, try setting alarm to wakeup.";
+            } else {
+                analysis += "You having good sleep, keep it up!";
+            }
+        }
+        return analysis;
     }
 
-    public void onClickTemp(View view) {
+    public void onClickDebugUnderSleep(View view) { addSleepRecord(view, 24000); }
+
+    public void onClickDebugAvgSleep(View view) { addSleepRecord(view, 30000); }
+
+    public void onClickDebugOverSleep(View view) { addSleepRecord(view, 32760); }
+
+    private void addSleepRecord(View view, int sec) {
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
         String email = sharedPref.getString("login_email", "No email");
-
-        SleepEntity sleepEntity = new SleepEntity(email, LocalDate.now(), LocalTime.now(), 32760);
-        // 7hrs = 25200 secs
-        // 9hrs = 32400 secs
-        // total <7hrs, >9hrs, avg
-
-        Log.d("SleepEntity", sleepEntity.toString());
+        SleepEntity sleepEntity = new SleepEntity(email, LocalDate.now(), LocalTime.now(), sec);
         dbService.sleepDao().insertAll(sleepEntity);
-    }
-
-    public void onClickTemp2(View view) {
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String email = sharedPref.getString("login_email", "No email");
-
-        SleepEntity sleepEntity = new SleepEntity(email, LocalDate.now(), LocalTime.now(), 24000);
-        // 7hrs = 25200 secs
-        // 9hrs = 32400 secs
-        // total <7hrs, >9hrs, avg
-
-        Log.d("SleepEntity", sleepEntity.toString());
-        dbService.sleepDao().insertAll(sleepEntity);
+        Snackbar.make(view, new StringBuffer("Added record."), Snackbar.LENGTH_SHORT).show();
     }
 
 }
